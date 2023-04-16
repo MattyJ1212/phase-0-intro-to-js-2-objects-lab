c3457e2ccb1a5e32fc91fae93f5fbc3cf431fc0d
MatthewJacquez <matt.cras@gmail.com>
2023-04-15 18:09:50 -0700

    commit tree 931d46e69a65cd43d8988a61ec59d5332226d8ee

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Matt",
+        address: {
+         street: "Broadway",
+            city: "New York",
+            state: "NY",
+            zip: "13456",
+    }
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee};
+
+newEmployee[key] = value;
+return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+ employee[key] = value;
+ return employee;
+
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newestEmployee = {...employee};
+
+    newestEmployee[key] = value;
+    return newestEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee.name;
+    return employee;
+}
